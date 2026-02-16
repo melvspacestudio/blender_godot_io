6d8d99ebcaee983fd5c35d34b07b311b15c0c98b
melvspace <amelvspace@gmail.com>
2026-02-17 00:12:34 +0300

    feat: implement material processor
    Implement logic in `material_processor.gd` to automatically save GLTF materials
    as external `.tres` resources and reuse existing ones.

--- godot/blender_godot_io_addon/processors/assets/material_processor.gd
+++ godot/blender_godot_io_addon/processors/assets/material_processor.gd
@@ -7,13 +7,13 @@ var material_dir: String = "assets/materials"
 func _get_material_dir() -> String:
 	if material_dir.begins_with("res://"):
 		return material_dir
-	
+		
 	if not material_dir.begins_with("/"):
 		var resource_dir = resource_path.replace("res://", "").split("/")
 		resource_dir.remove_at(resource_dir.size() - 1)
 		
 		return "res://" + "/".join(resource_dir)
-		
+
 	return material_dir
 
 func _pre_generate(state: GLTFState) -> Error:
@@ -21,6 +21,18 @@ func _pre_generate(state: GLTFState) -> Error:
 		push_warning("Material directory is not set for %s" % [self])
 		return ERR_SKIP
 	
-	print(state)
+	var materials = state.get_materials()
+	var json_materials = state.json["materials"]
+	
+	for i in len(materials):
+		var material = materials[i]
+		var name = json_materials[i]["name"]
 		
+		var path = _get_material_dir().path_join(name + ".tres")
+		if not ResourceLoader.exists(path):
+			ResourceSaver.save(material, path, ResourceSaver.FLAG_CHANGE_PATH)
+		
+		else:
+			material.take_over_path(path)	
+
 	return OK
